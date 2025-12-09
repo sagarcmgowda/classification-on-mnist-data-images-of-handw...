fa68976fe1d29b3ef21e1a18c938446d5c907540
Sagar cm <sagarcmgowda3111@gmail.com>
2025-12-09 14:54:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,28 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-model = tf.keras.models.load_model("mnist_model.h5")
+st.set_page_config(page_title="MNIST Digit Recognizer", layout="centered")
 
-st.title("Handwritten Digit Recognition")
+st.title("MNIST Digit Recognizer")
 
-file = st.file_uploader("Upload digit image", type=["png","jpg"])
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model("mnist_model.h5")
 
-if file:
-    img = Image.open(file).convert("L").resize((28,28))
-    img = np.array(img)/255.0
-    img = img.reshape(1,784)
+model = load_model()
 
-    pred = model.predict(img)
-    st.write("Prediction:", np.argmax(pred))
+uploaded_file = st.file_uploader("Upload a handwritten digit image", type=["png", "jpg", "jpeg"])
 
+if uploaded_file:
+    img = Image.open(uploaded_file).convert("L").resize((28, 28))
+    img = np.array(img)
+    img = 255 - img
+    img = img.reshape(1, 784).astype("float32") / 255.0
+
+    prediction = model.predict(img)
+    digit = np.argmax(prediction)
+    confidence = np.max(prediction)
+
+    st.image(uploaded_file, caption="Uploaded Image", width=150)
+    st.success(f"Prediction: **{digit}**")
+    st.write(f"Confidence: **{confidence:.2%}**")
